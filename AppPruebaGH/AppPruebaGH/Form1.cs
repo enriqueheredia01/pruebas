8589b0bccc478921b9379614be54c5bc532f76d2
Enrique Heredia Herrera <enriqueheredia.e@gmail.com>
2019-06-15 17:49:53 -0500

    modificado desde enriquelaptop

--- AppPruebaGH/AppPruebaGH/Form1.cs
+++ AppPruebaGH/AppPruebaGH/Form1.cs
@@ -21,5 +21,10 @@ namespace AppPruebaGH
         {
             MessageBox.Show("Hola");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola modificado");
+        }
     }
 }
