759e3698a2d877a5b6ad13b3a9b1ac279d102960
Enrique Heredia Herrera <enriqueheredia.e@gmail.com>
2019-06-15 18:51:52 -0500

    sfdfsdfd

--- AppPruebaGH/AppPruebaGH/Form1.cs
+++ AppPruebaGH/AppPruebaGH/Form1.cs
@@ -26,5 +26,10 @@ namespace AppPruebaGH
         {
             MessageBox.Show("Hola modificado");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
